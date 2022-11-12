bb0bf71e9feb725a0d9b3e02922b72cdda7fe18a
Dipanshu <dipanshudee12345@gmail.com>
2022-11-12 14:55:09 +0530

    Update P2PMarkets.tsx

--- src/pages/P2PMarkets.tsx
+++ src/pages/P2PMarkets.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import "./P2PMarkets.css";
+import Data from "../data.json"
 
 const P2PMarkets: FunctionComponent = () => {
   return (
@@ -13,96 +14,30 @@ const P2PMarkets: FunctionComponent = () => {
         src="../vuesaxlinearactivity.svg"
       />
       <div className="rectangle-div" />
-      <div className="frame-div">
-        <img className="vector-icon" alt="" src="../vector.svg" />
-        <div className="bitcoin-btc-div">Bitcoin (BTC)</div>
-        <div className="tvl-div">tvl</div>
-        <div className="price-div">Price</div>
-        <div className="popular-pairs-div">
-          <span className="t-span">p</span>
-          <span className="opular-pairs-span">opular pairs</span>
-        </div>
-        <img className="vector-icon1" alt="" src="../vector1.svg" />
-        <img className="bitcoin-sv-bsv" alt="" src="../bitcoin-sv-bsv.svg" />
-        <div className="rectangle-div1" />
-        <div className="rectangle-div2" />
-        <div className="div">$31,812.80</div>
-        <div className="div1">+10%</div>
-        <div className="div2">$60,000</div>
-        <img className="frame-icon" alt="" src="../frame-329.svg" />
-      </div>
-      <div className="frame-div1">
-        <img className="vector-icon" alt="" src="../vector2.svg" />
-        <div className="bitcoin-btc-div">Solana (SOL)</div>
-        <div className="tvl-div">tvl</div>
-        <div className="price-div">Price</div>
-        <div className="popular-pairs-div">
-          <span className="t-span">p</span>
-          <span className="opular-pairs-span">opular pairs</span>
-        </div>
-        <img className="vector-icon3" alt="" src="../vector3.svg" />
-        <img className="bitcoin-sv-bsv1" alt="" src="../bitcoin-sv-bsv1.svg" />
-        <div className="rectangle-div1" />
-        <div className="rectangle-div2" />
-        <div className="div">$32.83</div>
-        <div className="div4">-12.32%</div>
-        <div className="div2">$60,000</div>
-        <img className="frame-icon" alt="" src="../frame-3291.svg" />
-      </div>
-      <div className="frame-div2">
-        <img className="vector-icon" alt="" src="../vector4.svg" />
-        <div className="bitcoin-btc-div">Ethereum (ETH)</div>
-        <div className="tvl-div">tvl</div>
-        <div className="price-div">Price</div>
-        <div className="popular-pairs-div">
-          <span className="t-span">p</span>
-          <span className="opular-pairs-span">opular pairs</span>
-        </div>
-        <img className="vector-icon3" alt="" src="../vector5.svg" />
-        <img className="bitcoin-sv-bsv2" alt="" src="../bitcoin-sv-bsv2.svg" />
-        <div className="rectangle-div1" />
-        <div className="rectangle-div2" />
-        <div className="div">$1,466.45</div>
-        <div className="div4">-11.93%</div>
-        <div className="div2">$60,000</div>
-        <img className="frame-icon" alt="" src="../frame-3291.svg" />
-      </div>
-      <div className="frame-div3">
-        <img className="vector-icon" alt="" src="../vector6.svg" />
-        <div className="bitcoin-btc-div">Binance USD (BUSD)</div>
-        <div className="tvl-div">tvl</div>
-        <div className="price-div">Price</div>
-        <div className="popular-pairs-div">
-          <span className="t-span">p</span>
-          <span className="opular-pairs-span">opular pairs</span>
-        </div>
-        <img className="vector-icon1" alt="" src="../vector7.svg" />
-        <img className="bitcoin-sv-bsv2" alt="" src="../bitcoin-sv-bsv2.svg" />
-        <div className="rectangle-div1" />
-        <div className="rectangle-div2" />
-        <div className="div">$1.00</div>
-        <div className="div1">+0.26%</div>
-        <div className="div2">$60,000</div>
-        <img className="frame-icon" alt="" src="../frame-3293.svg" />
-      </div>
-      <div className="frame-div4">
-        <img className="vector-icon" alt="" src="../vector8.svg" />
-        <div className="bitcoin-btc-div">Shiba Inu (SHIB)</div>
-        <div className="tvl-div">tvl</div>
-        <div className="price-div">Price</div>
-        <div className="popular-pairs-div">
-          <span className="t-span">p</span>
-          <span className="opular-pairs-span">opular pairs</span>
-        </div>
-        <img className="vector-icon3" alt="" src="../vector9.svg" />
-        <img className="bitcoin-sv-bsv4" alt="" />
-        <div className="rectangle-div1" />
-        <div className="rectangle-div2" />
-        <div className="div">$0.00000001948</div>
-        <div className="div13">-8.1%</div>
-        <div className="div2">$60,000</div>
-        <img className="frame-icon" alt="" src="../frame-3294.svg" />
-      </div>
+      {
+        Data.map(curr=> {
+          return(
+            <div className="frame-div">
+            <img className="vector-icon" alt="" src={curr.images} />
+            <div className="bitcoin-btc-div">{curr.name}</div>
+            <div className="tvl-div">tvl</div>
+            <div className="price-div">Price</div>
+            <div className="popular-pairs-div">
+              <span className="t-span">p</span>
+              <span className="opular-pairs-span">opular pairs</span>
+            </div>
+            <img className="vector-icon1" alt="" src={curr.subImages} />
+            <img className="bitcoin-sv-bsv" alt="" src={curr.subImagesnew} />
+            <div className="rectangle-div1" />
+            <div className="rectangle-div2" />
+            <div className="div">{curr.Price}</div>
+            <div className="div1">{curr.Percentage}</div>
+            <div className="div2">{curr.subPrice}</div>
+            <img className="frame-icon" alt="" src={curr.iconImages} />
+          </div>
+          )
+        })
+}
       <img className="vector-icon10" alt="" src="../vector10.svg" />
       <img className="solana-sol-icon" alt="" src="../solana-sol.svg" />
       <img className="bitcoin-sv-bsv5" alt="" src="../bitcoin-sv-bsv4.svg" />
